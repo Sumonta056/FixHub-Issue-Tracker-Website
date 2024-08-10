cf433b9adcda21a96af7a5e9db90ef1c043d7bb6
sumonta056 <sumontasaha80@gmail.com>
2024-08-10 10:59:15 +0600

    feat : build latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import { Heading } from "@radix-ui/themes";
-import Pagination from "@/app/components/Pagination";
+import LatestIssue from "./dashboard/LatestIssue";
 
-export default function Home({ searchParams }: { searchParams: { page: string } }) {
+export default function Home() {
   return (
-    <div className="mx-auto max-w-4xl flex flex-col space-y-3">
-      <Heading color="indigo">Issue Dashboard</Heading>
-
-      <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
+    <div className="">
+      <LatestIssue />
     </div>
   );
 }
